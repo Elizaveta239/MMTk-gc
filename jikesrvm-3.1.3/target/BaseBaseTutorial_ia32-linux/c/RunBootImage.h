d64a62bbe54f1519b63899882f9e8f3a1f06fa91
elizabeth <elizabeth@elizabeth-VirtualBox.(none)>
2013-08-14 09:49:47 -0400

    Premiliminaries are done

---
+++ jikesrvm-3.1.3/target/BaseBaseTutorial_ia32-linux/c/RunBootImage.h
@@ -0,0 +1,8 @@
+
+      const unsigned heap_default_initial_size = 20*1024*1024;
+      const unsigned heap_default_maximum_size = 100*1024*1024;
+      const char *rvm_version = "Jikes RVM 3.1.3 (revision )";
+      const char *rvm_configuration = "BaseBaseTutorial";
+      const char *rvm_host_configuration = "/home/elizabeth/sum13/ideal/jikesrvm-3.1.3/build/configs/BaseBaseTutorial.properties";
+      const char *rvm_target_configuration = "/home/elizabeth/sum13/ideal/jikesrvm-3.1.3/build/targets/ia32-linux.properties";
+    
